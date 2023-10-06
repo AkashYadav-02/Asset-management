c4da8bce83ceff8730d2054f127d08aff0e3c364
Tanish2001 <bathamtanish@gmail.com>
2023-10-06 16:29:54 +0530

    changes added

--- src/app/navbar-after-home-page/navbar-after-home-page.component.ts
+++ src/app/navbar-after-home-page/navbar-after-home-page.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit, Input, HostListener, ElementRef, ViewChild, Renderer2} from '@angular/core';
-
+import { Component, OnInit, Input, HostListener, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RedirectMenuService } from 'src/services/redirect-menu.service';
-
 declare var $: any;
 
 @Component({
@@ -11,100 +9,138 @@ declare var $: any;
   templateUrl: './navbar-after-home-page.component.html',
   styleUrls: ['./navbar-after-home-page.component.scss']
 })
-export class NavbarAfterHomePageComponent implements OnInit{
-  isExpand=false;
-  isNotiExpand=false;
-  isProfileExpand=false;
+export class NavbarAfterHomePageComponent implements OnInit {
+  isExpand = false;
+  isNotiExpand = false;
+  isProfileExpand = false;
 
-  @Input() navtheme?:string;
+  @Input() navtheme?: string;
 
-  txtcolor='';
-  aurologo='';
-  bgcolor='';
-  like='';
-  noti='';
-  sett='';
-  sideburger=''
+  txtcolor = '';
+  aurologo = '';
+  bgcolor = '';
+  like = '';
+  noti = '';
+  sett = '';
+  sideburger = ''
+  // const element = document.getElementById('yourElementId'); 
+  // x = this.element.scrollHeight;
 
   ngOnInit(): void {
-    if (this.navtheme =='blacktheme'){
-      this.txtcolor='white';
-      this.bgcolor='rgba(0, 0, 0, 0)'
-      this.aurologo='../../assets/Aurionpro.png';
-      this.like='../../assets/logos/Like.png';
-      this.noti='../../assets/logos/Notification.png';
-      this.sett='../../assets/logos/Settings.png';
-      this.sideburger='../../assets/logos/white-burger.png';
-    } else if (this.navtheme =='whitetheme'){
-      this.txtcolor='black';
-      this.bgcolor='white'
-      this.aurologo='../../assets/logos/logo.png';
-      this.like='../../assets/logos/Like2.png';
-      this.noti='../../assets/logos/Notification2.png';
-      this.sett='../../assets/logos/Settings2.png';
-      this.sideburger='../../assets/logos/black-burger.png';
+    if (this.navtheme == 'blacktheme') {
+      // TypeScript code
+      window.addEventListener('scroll', () => {
+        const navbar = document.getElementById('car-subs-nav');
+
+        if (navbar) {
+          const scrollThreshold = 10;
+
+          if (window.scrollY > scrollThreshold) {
+            this.txtcolor = 'black';
+            this.bgcolor = 'white'
+            this.aurologo = '../../assets/logos/logo.png';
+            this.like = '../../assets/logos/Like2.png';
+            this.noti = '../../assets/logos/Notification2.png';
+            this.sett = '../../assets/logos/Settings2.png';
+            this.sideburger = '../../assets/logos/black-burger.png';
+            navbar.style.width = '100%';
+            navbar.style.paddingLeft = '65px'
+            navbar.style.paddingRight = '65px'
+          } else {
+            // navbar.style.backgroundColor = '#333'; // Initial color
+            this.txtcolor = 'white';
+            this.bgcolor = 'rgba(0, 0, 0, 0)'
+            this.aurologo = '../../assets/Aurionpro.png';
+            this.like = '../../assets/logos/Like.png';
+            this.noti = '../../assets/logos/Notification.png';
+            this.sett = '../../assets/logos/Settings.png';
+            this.sideburger = '../../assets/logos/white-burger.png';
+            navbar.style.width = '91%';
+            navbar.style.paddingLeft = '0px'
+            navbar.style.paddingRight = '0px'
+          }
+        }
+      });
+
+      this.txtcolor = 'white';
+      this.bgcolor = 'rgba(0, 0, 0, 0)'
+      this.aurologo = '../../assets/Aurionpro.png';
+      this.like = '../../assets/logos/Like.png';
+      this.noti = '../../assets/logos/Notification.png';
+      this.sett = '../../assets/logos/Settings.png';
+      this.sideburger = '../../assets/logos/white-burger.png';
+    } else if (this.navtheme == 'whitetheme') {
+      this.txtcolor = 'black';
+      this.bgcolor = 'white'
+      this.aurologo = '../../assets/logos/logo.png';
+      this.like = '../../assets/logos/Like2.png';
+      this.noti = '../../assets/logos/Notification2.png';
+      this.sett = '../../assets/logos/Settings2.png';
+      this.sideburger = '../../assets/logos/black-burger.png';
     }
+
   }
 
-  constructor(private router:Router,
+  constructor(private router: Router,
     private fb: FormBuilder,
-    private redirectMenu : RedirectMenuService,
-     ) {}
+    private redirectMenu: RedirectMenuService,
+  ) { }
 
-    //  @HostListener('window:scroll', ['$event']) 
-    // scrollHandler(event:Event) {
-    //   console.debug("Scroll Event");
-    //   this.navtheme='';
-    // }
+  //  @HostListener('window:scroll', ['$event']) 
+  // scrollHandler(event:Event) {
+  //   console.debug("Scroll Event");
+  //   this.navtheme='';
+  // }
 
-  expandSideBar(){
-    if (this.isExpand){
-      this.isExpand=false
-    }else{
-      this.isExpand=true
+  expandSideBar() {
+    if (this.isExpand) {
+      this.isExpand = false
+    } else {
+      this.isExpand = true
     }
-    this.isNotiExpand=false
-    this.isProfileExpand=false
+    this.isNotiExpand = false
+    this.isProfileExpand = false
   }
-  expandNotiBar(){
-    if (this.isNotiExpand){
-      this.isNotiExpand=false
-    }else{
-      this.isNotiExpand=true
+  expandNotiBar() {
+    if (this.isNotiExpand) {
+      this.isNotiExpand = false
+    } else {
+      this.isNotiExpand = true
     }
-    this.isProfileExpand=false
-    this.isExpand=false
+    this.isProfileExpand = false
+    this.isExpand = false
   }
-  expandProfileBar(){
-    if (this.isProfileExpand){
-      this.isProfileExpand=false
-    }else{
-      this.isProfileExpand=true
-      this.isNotiExpand=false
-      this.isExpand=false
-    }}
-
-  setBackground(color:string){
-    if(color == "black"){
-      this.txtcolor='black';
-      this.bgcolor='white'
-      this.aurologo='../../assets/logos/logo.png';
-      this.like='../../assets/logos/Like2.png';
-      this.noti='../../assets/logos/Notification2.png';
-      this.sett='../../assets/logos/Settings2.png';
-      this.sideburger='../../assets/logos/black-burger.png';
-    }else{
-      this.txtcolor='white';
-      this.bgcolor='rgba(0, 0, 0, 0)'
-      this.aurologo='../../assets/Aurionpro.png';
-      this.like='../../assets/logos/Like.png';
-      this.noti='../../assets/logos/Notification.png';
-      this.sett='../../assets/logos/Settings.png';
-      this.sideburger='../../assets/logos/white-burger.png';
+  expandProfileBar() {
+    if (this.isProfileExpand) {
+      this.isProfileExpand = false
+    } else {
+      this.isProfileExpand = true
+      this.isNotiExpand = false
+      this.isExpand = false
     }
   }
 
-  redirect(path : string){
+  setBackground(color: string) {
+    if (color == "black") {
+      this.txtcolor = 'black';
+      this.bgcolor = 'white'
+      this.aurologo = '../../assets/logos/logo.png';
+      this.like = '../../assets/logos/Like2.png';
+      this.noti = '../../assets/logos/Notification2.png';
+      this.sett = '../../assets/logos/Settings2.png';
+      this.sideburger = '../../assets/logos/black-burger.png';
+    } else {
+      this.txtcolor = 'white';
+      this.bgcolor = 'rgba(0, 0, 0, 0)'
+      this.aurologo = '../../assets/Aurionpro.png';
+      this.like = '../../assets/logos/Like.png';
+      this.noti = '../../assets/logos/Notification.png';
+      this.sett = '../../assets/logos/Settings.png';
+      this.sideburger = '../../assets/logos/white-burger.png';
+    }
+  }
+
+  redirect(path: string) {
     this.redirectMenu.redirectTo(path);
   }
 
