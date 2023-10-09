69bb6f8bd11abf79145c35fed1ecad27d436a114
Tejas Prabhu <tejas.prabhu@aurionpro.com>
2023-10-09 15:21:26 +0530

    Cred Info Changes

--- src/app/apply-now-flow1/apply-now-flow1.component.ts
+++ src/app/apply-now-flow1/apply-now-flow1.component.ts
@@ -97,7 +97,7 @@ export class ApplyNowFlow1Component implements OnInit {
     // this.http.put('../../assets/JSONfiles/workspace.json');
 
     
-    this.redirectMenu.redirectWithdata('work-space', data);
+   // this.redirectMenu.redirectWithdata('work-space', data);
     this.redirectMenu.redirectTo('credit-info');
   }
 
