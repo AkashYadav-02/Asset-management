7111d730c4f7ac939bfb4322c68cf057206876d7
KHUSHI PATEL <khushi.patel@asl.mum.sst>
2023-10-10 11:50:32 +0530

    apply now

--- src/app/apply-now-flow1/apply-now-flow1.component.ts
+++ src/app/apply-now-flow1/apply-now-flow1.component.ts
@@ -1,9 +1,6 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { leftcard } from '../carddetails';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 // import { Router } from '@angular/router';
 import { Router, NavigationEnd } from '@angular/router';
 import { RedirectMenuService } from 'src/services/redirect-menu.service';
@@ -13,7 +10,6 @@ import { RedirectMenuService } from 'src/services/redirect-menu.service';
   styleUrls: ['./apply-now-flow1.component.scss']
 })
 
-<<<<<<< HEAD
 export class ApplyNowFlow1Component {
   lastName: string = '';
   driversLicense: string = '';
@@ -24,7 +20,7 @@ export class ApplyNowFlow1Component {
   fname: string = '';
   lname: string = '';
   mname: string = '';
-  dob: string = '';
+  dob: string = 'date';
   mobile: string = '';
   home: string = '';
   email: string = '';
@@ -44,17 +40,11 @@ export class ApplyNowFlow1Component {
   workcity: string = '';
   workstate: string = '';
   workzip: string = '';
-=======
-export class ApplyNowFlow1Component implements OnInit {
-  applicationId: string = '';
->>>>>>> a2d2b0db11c1c35f7880c01b12e3541e8755818d
 
-  leftcard = leftcard;
-  myObserver;
-  currentUrl: any;
-  techDetailsParameter: any;
+  myObserver ;
+  currentUrl : any;
+  techDetailsParameter : any;
 
-<<<<<<< HEAD
 //  lastname: string = "Aurionpro";
   // driversLicense: string = "1223@"; 
  
@@ -64,33 +54,18 @@ export class ApplyNowFlow1Component implements OnInit {
   constructor(private router:Router,private fb: FormBuilder,
     private redirectMenu : RedirectMenuService,
      ) {
-=======
-  lineItem: any;
->>>>>>> a2d2b0db11c1c35f7880c01b12e3541e8755818d
 
-  constructor(private router: Router, private fb: FormBuilder,private http: HttpClient,
-    private redirectMenu: RedirectMenuService,
-  ) {
-
-    this.myObserver = this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.currentUrl = event.url;
+      this.myObserver = this.router.events.subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          this.currentUrl = event.url;
         const navigation = this.router.getCurrentNavigation();
         if (navigation?.extras.state) {
           this.techDetailsParameter = navigation.extras.state;
+        } 
         }
-      }
-      
-      
-    });
-  }
-
-  ngOnInit(): void {
-    this.generateRandomApplicationId();
-  }
+      });
 
 
-<<<<<<< HEAD
      }
   carDetails=[
     {
@@ -133,7 +108,11 @@ export class ApplyNowFlow1Component implements OnInit {
     }
 
   }
-
+  
+  
+  // onSubmit(){
+  //   this.router.redirectto()
+  // }
   radioChanged(event: Event): void {
     const radio = event.target as HTMLInputElement;
     if (radio.checked) {
@@ -180,75 +159,23 @@ export class ApplyNowFlow1Component implements OnInit {
   turnRed(){
     if (this.heartURL==this.white){
       this.heartURL=this.red
-=======
-  // carDetails=[
-  //   {
-  //     name:"2023 Toyota Fortuner",
-  //     price: "61,490",
-  //     imgUrl: "../../assets/Toyota-Fortuner-110120211829 2.png",
-  //   },
-  // ]
-  white = "../../../assets/logos/heart.png";
-  red = "../../../assets/logos/heartred.png"
-  heartURL = this.white;
-  turnRed() {
-    if (this.heartURL == this.white) {
-      this.heartURL = this.red
->>>>>>> a2d2b0db11c1c35f7880c01b12e3541e8755818d
     }
-    else if (this.heartURL == this.red) {
-      this.heartURL = this.white
+    else if (this.heartURL==this.red){
+      this.heartURL=this.white
     }
   }
-
-  generateRandomApplicationId(): void {
-    const randomPart = Math.floor(Math.random() * 10000).toString().padStart(4, '0'); // Generates a random 4-digit number
-    this.applicationId = `UNE${randomPart}`;
-    this.lineItem = this.applicationId;
-    console.log("line", this.lineItem);
-  }
-
-
-  redirect(path: string) {
+  redirect(path : string){
     this.redirectMenu.redirectTo(path);
   }
 
-  saveProcceed() {
-    let data = {
-      username: "Brock",
-      specs: this.techDetailsParameter,
-      applicationId: this.applicationId,
-      carddetails: this.leftcard
-      // emiDetails : 
-    }
-    // console.log("y", data);
-    let jsonData = {
-      appID: this.applicationId,
-      customerName: "John",
-      carSelection: this.techDetailsParameter.name,
-      financing: this.techDetailsParameter.price,
-      status: "Pending"
-    }
-
-    // let data = {
-    //   username: "John",
-    //   specs: this.techDetailsParameter,
-    //   applicationId: this.applicationId,
-    //   carddetails: this.leftcard,
-    //   jsonData: jsonData 
-    //   // emiDetails : 
-    // }
-    // console.log("Jason",jsonData);
-    // this.http.put('../../assets/JSONfiles/workspace.json');
-
-    
-  //  this.redirectMenu.redirectWithdata('work-space', );
-    this.redirectMenu.redirectWithdata('credit-info',data);
+  redirectToForgotPassPage(){
+    this.router.navigate(['credit-info']);
+  
   }
-
 }
 
+ 
 
+ 
 
-
-
+ 
